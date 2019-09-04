0fff1744964f45aff61150459feb78c749d4c8e7
Kwon-Young Choi <kwon-young.choi@hotmail.fr>
2019-09-04 17:50:51 +0200

    structured comments using PlDoc

--- cond.pl
+++ cond.pl
@@ -1,7 +1,28 @@
 :- module(cond, [noCond/1, condSegH/1, condSegV/1]).
 
-noCond(_).
+%% noCond(+El) is det
+%
+% The noCond/1 predicate always succeed regardless of El.
+%
+% * noCond(seg(h, _, _, _))
+%   (`true.`)
+%
+% * noCond(seg(v, _, _, _))
+%   (`true.`)
+%
+% @param El Unused element.
+noCond(_El).
 
+%% condSegH(+Seg:seg) is semidet.
+%
+% Assert that input segment is horizontal.
+%
+% @param Seg Input segment.
 condSegH(seg(h, _, _, _)).
 
+%% condSegV(+Seg:seg) is semidet.
+%
+% Assert that input segment is vertical.
+%
+% @param Seg Input segment.
 condSegV(seg(v, _, _, _)).
