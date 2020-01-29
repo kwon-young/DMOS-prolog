5d35f3de81707e87ea9e50dfd84b3a7ac396a6d4
Kwon-Young Choi <kwon-young.choi@hotmail.fr>
2020-01-29 23:13:53 +0100

    Module level documentation and completed condSegV and condSegH doc

--- cond.pl
+++ cond.pl
@@ -1,13 +1,25 @@
 :- module(cond, [noCond/1, condSegH/1, condSegV/1]).
 
+/** <module> Conditions Module
+ *
+ * Helper predicates for terminal consumption conditions.
+ * These predicates are used together with the term_seg or term_cc EPF
+ * operator to specify conditions that an element should fullfill to be
+ * consumed.
+ *
+ * By convention, all condition predicates start with a 'cond'.
+ * These predicates always take at least a single parameter which is the
+ * current element considered.
+ *
+ * @author Kwon-Young Choi
+ * @version 0.1
+ */
+
 %% noCond(+El) is det
 %
 % The noCond/1 predicate always succeed regardless of El.
 %
-% * noCond(seg(h, _, _, _))
-%   (`true.`)
-%
-% * noCond(seg(v, _, _, _))
+% * noCond(El)
 %   (`true.`)
 %
 % @param El Unused element.
@@ -17,12 +29,24 @@ noCond(_El).
 %
 % Assert that input segment is horizontal.
 %
+% * condSegH(seg(h, _, _, _))
+%   (`true.`)
+%
+% * condSegH(seg(v, _, _, _))
+%   (`false.`)
+%
 % @param Seg Input segment.
 condSegH(seg(h, _, _, _)).
 
 %% condSegV(+Seg:seg) is semidet.
 %
 % Assert that input segment is vertical.
+%
+% * condSegV(seg(h, _, _, _))
+%   (`false.`)
+%
+% * condSegV(seg(v, _, _, _))
+%   (`true.`)
 %
 % @param Seg Input segment.
 condSegV(seg(v, _, _, _)).
