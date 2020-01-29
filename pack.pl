eba3964e5120812ade9bd307da9503a721b71ebc
Kwon-Young Choi <kwon-young.choi@hotmail.fr>
2020-01-29 23:13:09 +0100

    Specify edcg dependency for package.

---
+++ pack.pl
@@ -0,0 +1,6 @@
+name(dmos_prolog).
+title('DMOS swi-prolog implementation').
+version('0.1').
+author('Kwon-Young Choi', 'kwon-young.choi@hotmail.fr').
+home('https://github.com/kwon-young/DMOS-prolog').
+requires(edcg).
